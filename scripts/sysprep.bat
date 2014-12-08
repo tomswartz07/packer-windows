4565f4ca6d4dcdaf9ef4d63d2af0e6dc367afbec
Tom Swartz <tom@pennmanor.net>
2014-12-08 14:42:27 -0500

    Fix Sysprep command
    Use proper path for Sysprep unattend file
    
    Also disable Chef install, as it's not needed

--- scripts/sysprep.bat
+++ scripts/sysprep.bat
@@ -1,4 +1,4 @@
 rem Sysprep the device
 rem Runs sysprep with the unattend file for answers
 
-cmd /c "c:\windows\system32\sysprep\sysprep /generalize /oobe /shutdown /unattend:C:\Windows\Temp\answer_files\sysprep\81\unattendSysprep.xml"
+cmd /c "c:\windows\system32\sysprep\sysprep /generalize /oobe /shutdown /unattend:A:\unattendSysprep.xml"
