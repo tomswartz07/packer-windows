5ec4679a30d473ea6830ce86e8431061b29d2bf7
Tom Swartz <tom.swartz07@gmail.com>
2015-03-19 10:13:05 -0400

    Temporarily Override PowerShell Execution Policy
    Override the execution policy during the build phase to assure that
    software updates are installed as expected via PS scripts.

--- scripts/pennmanorAD.bat
+++ scripts/pennmanorAD.bat
@@ -27,6 +27,11 @@ echo "ownCloud Setup Complete"
 start %DeploymentServer%\comets\comets.bat
 echo "Comets WiFi Setup Complete"
 
+:: Set up Powershell execution policy for current user (admin)
+:: This allows provisioning scripts to be run as local admin during the setup phase
+:: See: https://technet.microsoft.com/en-us/library/hh849812.aspx
+powershell -Command "Set-ExecutionPolicy Bypass -Scope CurrentUser -Force
+
 :: Re-Run Windows Updates
 :: This should be run LAST, so that any previous software installs (especially Office) get updates
 :: Further, the win-updates script on %DeploymentServer is modified so that SSH doesn't get reinstalled.
