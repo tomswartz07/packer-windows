25a17b0d2b9ee26857acaef4bd4974e374f1e15a
Tom Swartz <tom@pennmanor.net>
2015-04-06 16:14:14 -0400

    Add script to remove Metro Apps
    Script will remove defined Metro Apps

---
+++ scripts/windowsApps.ps1
@@ -0,0 +1,30 @@
+# Remove the default Windows Apps from the image
+#
+# Seriously, who uses these dumb things?
+#
+# To get the name of the packages:
+# - Dism.exe /online /get-ProvisionedAppxPackages 
+#
+# Alternately, on a live machine, you can run:
+# - Dism.exe /online /Remove-ProvisionedAppxPackage /PackageName:microsoft.windowsphotos_16.4.4396.311_x64__8wekyb3d8bbwe
+# - Dism.exe /online /Remove-ProvisionedAppxPackage /PackageName:Microsoft.ZuneVideo_1.5.802.0_x64__8wekyb3d8bbwe
+#
+$AppList = "Microsoft.Reader",
+           "Microsoft.WindowsScan",
+           "microsoft.windowscommunicationsapps",
+           "Microsoft.WindowsReadingList",
+           "Microsoft.XboxLIVEGames",
+           "Microsoft.ZuneMusic",
+           "microsoft.windowsphotos",
+           "Microsoft.bingTravel",
+           "Microsoft.bingMaps",
+           "Microsoft.bingFinance",
+           "Microsoft.bingFoodAndDrink",
+           "Microsoft.bingHealthAndFitness",
+           "Microsoft.ZuneVideo"
+
+ForEach ($App in $AppList) {
+   $AppxPackage = Get-AppxProvisionedPackage -online | Where {$_.DisplayName -eq $App}
+   Remove-AppxProvisionedPackage -online -packagename ($AppxPackage.PackageName) -ErrorAction SilentlyContinue
+   Remove-AppxPackage ($AppxPackage.PackageName) -ErrorAction SilentlyContinue
+}
