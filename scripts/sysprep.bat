889cdf57d4526abed9a0c5d6c6fe4e91f8387917
Tom Swartz <tom.swartz07@gmail.com>
2015-04-09 10:52:50 -0400

    Revert "Update Sysprep Info"
    This reverts commit 5b324c22aa15694ee0e84b95e8b81d4c6b01e875.

--- scripts/sysprep.bat
+++ scripts/sysprep.bat
@@ -1,4 +1,4 @@
-:: Sysprep the device
-:: Runs sysprep with the unattend file for answers
+rem Sysprep the device
+rem Runs sysprep with the unattend file for answers
 
 cmd /c "c:\windows\system32\sysprep\sysprep /generalize /oobe /quit /unattend:A:\unattendSysprep.xml"
