6fe53a9612b7cbc2254e7fc93d6e439be6e728ab
Tom Swartz <tom@pennmanor.net>
2015-04-13 10:06:56 -0400

    Update Sysprep answers
    Properly identify the cleanup commands and make the Support information
    a bit more concise.

--- scripts/sysprep.bat
+++ scripts/sysprep.bat
@@ -1,4 +1,4 @@
-rem Sysprep the device
-rem Runs sysprep with the unattend file for answers
+:: Sysprep the device
+:: Runs sysprep with the unattend file for answers
 
 cmd /c "c:\windows\system32\sysprep\sysprep /generalize /oobe /quit /unattend:A:\unattendSysprep.xml"
