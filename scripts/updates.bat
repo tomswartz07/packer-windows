15d92341a8c35fa1ac864f12b49d29d4c55f4934
Tom Swartz <tom@pennmanor.net>
2015-07-09 14:31:09 -0400

    Break out the Update script
    Move the WinUpdates script to it's own provisioner with a delay of 5m to
    allow for the VM to restart and install any updates.
    
    Delay might need be increased depending on update quantity.
    Currently 5 minutes is ample time for reboots.

--- scripts/updates.bat
+++ scripts/updates.bat
@@ -1,4 +1,3 @@
-@echo off
 :: Set up Powershell execution policy for current user (admin)
 :: This allows provisioning scripts to be run as local admin during the setup phase
 :: See: https://technet.microsoft.com/en-us/library/hh849812.aspx
@@ -8,7 +7,8 @@ powershell -Command "Set-ExecutionPolicy Bypass -Scope CurrentUser -Force"
 :: This should be run LAST, so that any previous software installs (especially Office) get updates
 :: Further, the win-updates script on %DeploymentServer is modified so that SSH doesn't get reinstalled.
 echo %time% :: Starting WinUpdates powershell script in IT Tools Folder
-powershell -Command "C:\IT Tools\win-updates.ps1"
+set Command = "C:\IT Tools\win-updates.ps1"
+powershell -Command "%Command%"
 
 :: One last restart before continuing with the process.
 echo %time% :: Windows Updates complete, restarting.
