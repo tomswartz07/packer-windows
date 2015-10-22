db21989281d70db93cf0bca1871d87a4c30ac993
Tom Swartz <tom@pennmanor.net>
2015-10-22 10:54:10 -0400

    Update Packer script to use new provisioners
    Packer now has `windows-restart` provisioner that handles restarts much
    more gracefully than the previous methods.

--- scripts/updates.bat
+++ scripts/updates.bat
@@ -1,3 +1,5 @@
+@echo off
+
 :: Set up Powershell execution policy for current user (admin)
 :: This allows provisioning scripts to be run as local admin during the setup phase
 :: See: https://technet.microsoft.com/en-us/library/hh849812.aspx
@@ -5,11 +7,10 @@ powershell -Command "Set-ExecutionPolicy Bypass -Scope CurrentUser -Force"
 
 :: Re-Run Windows Updates
 :: This should be run LAST, so that any previous software installs (especially Office) get updates
-:: Further, the win-updates script on %DeploymentServer is modified so that SSH doesn't get reinstalled.
+:: Use the pennmanorAD.bat deployed version of script, as it prevents SSH reinstall
 echo %time% :: Starting WinUpdates powershell script in IT Tools Folder
-set Command = "C:\IT Tools\win-updates.ps1"
-powershell -Command "%Command%"
+powershell -Command "C:\IT Tools\win-updates.ps1"
 
 :: One last restart before continuing with the process.
 echo %time% :: Windows Updates complete, restarting.
-shutdown.exe /r /f /t 30
+echo %time% :: Handing Restart process off to Packer.IO. Restarting...
