1a488e35c0135270b6f607eb121daedeab5153d3
Tom Swartz <tom@pennmanor.net>
2016-07-06 16:53:44 -0400

    Fix Sysprep call
    Fixes a regression in the Sysprep.bat file that caused the Sysprep step
    to hang indefinitely.

--- scripts/sysprep.bat
+++ scripts/sysprep.bat
@@ -2,5 +2,5 @@
 :: Runs sysprep with the unattend file for answers
 
 echo %time% :: Starting Sysprep
-start /wait "C:\Windows\System32\Sysprep\sysprep.exe /generalize /oobe /quit"
+cmd /c "C:\Windows\System32\Sysprep\sysprep.exe /generalize /oobe /quit /unattend:A:\unattendSysprep.xml"
 echo %time% :: Finished Sysprep
