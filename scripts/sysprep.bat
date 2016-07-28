c42cd517bb8beb39663798f2687a48587e963cc3
Tom Swartz <tom@pennmanor.net>
2016-07-28 08:12:50 -0400

    Try using Microsoft provided KMS keys
    Add extra Sysprep options to prevent GUI from showing, and log
    everything instead.

--- scripts/sysprep.bat
+++ scripts/sysprep.bat
@@ -2,5 +2,5 @@
 :: Runs sysprep with the unattend file for answers
 
 echo %time% :: Starting Sysprep
-cmd /c "C:\Windows\System32\Sysprep\sysprep.exe /generalize /oobe /quit /unattend:A:\unattendSysprep.xml"
+cmd /c "C:\Windows\System32\Sysprep\sysprep.exe /generalize /quiet /oobe /quit /unattend:A:\unattendSysprep.xml"
 echo %time% :: Finished Sysprep
