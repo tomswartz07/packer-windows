906de66fb0cb211f3b33c70d025604ad7149b921
Tom Swartz <tom@pennmanor.net>
2016-08-01 12:16:41 -0400

    Tweak WindowsUpdate script
    Modify the 'admin' user to temporarily run Powershell Scripts
    unrestricted during the install process.
    
    This prevents Windows 10 from blocking access to the Update CMDlets.
    
    As noted in this ServerFault discussion:
    https://serverfault.com/questions/696689/cannot-set-powershell-executionpolicy-for-currentuser

--- scripts/updates.bat
+++ scripts/updates.bat
@@ -5,11 +5,11 @@
 :: See: https://technet.microsoft.com/en-us/library/hh849812.aspx
 :: There seems to be a bug for Windows 10:
 :: https://serverfault.com/questions/696689/cannot-set-powershell-executionpolicy-for-currentuser
-powershell -Command "Set-ExecutionPolicy Bypass -Scope CurrentUser -Force"
+powershell -Command "Set-ExecutionPolicy Unrestricted -Scope CurrentUser -Force"
 
 :: Re-Run Windows Updates
 :: This should be run LAST, so that any previous software installs (especially Office) get updates
-:: Use the pennmanorAD.bat deployed version of script, as it prevents SSH reinstall
+:: Use the locally deployed version of script, as it is modified to prevent SSH reinstall
 echo %time% :: Starting WinUpdates powershell script in IT Tools Folder
 powershell -Command "& 'C:\IT Tools\win-updates.ps1'"
 
